eaf527e1586d1d42f8b7c71639353261ccc54d46
FoodBreakPedro <pedro.r.zabala@gmail.com>
2022-05-30 17:34:00 -0400

    build deploy

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,9 @@
 import Header from "./components/header/header";
 import About from "./components/about/about";
-import Experience from "./components/experience/experience";
+//import Experience from "./components/experience/experience";
 import Contact from "./components/contact/contact";
 import Navbar from "./components/nav/nav";
-import Portfolio from "./components/portfolio/portfolio";
+//import Portfolio from "./components/portfolio/portfolio";
 import Footer from "./components/footer/footer";
 
 function WebContent() {
