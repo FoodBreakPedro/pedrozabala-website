e475b190329707dba1366da48b7d21f91f53e23d
FoodBreakPedro <pedro.r.zabala@gmail.com>
2022-05-30 17:06:55 -0400

    commit of extra files

---
+++ src/components/experience/experience.jsx
@@ -0,0 +1,12 @@
+import "./experience.css";
+import Qualifications from "../qualifications/qualifications";
+
+function Experience() {
+  return (
+    <div id="experience" className="container experience-container">
+       
+      <Qualifications />
+    </div>
+  );
+}
+export default Experience;
